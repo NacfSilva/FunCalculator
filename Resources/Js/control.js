9fc55bc215c2d0ef54bb3967c9a834a4a3b6e9ff
NacfSilva <nacf.silva@gmail.com>
2020-02-06 23:26:28 +0000

    Add files via upload

--- Resources/Js/control.js
+++ Resources/Js/control.js
@@ -1,13 +1,46 @@
-// number max that calculater can calculate 2,147,483,647 after that right "overflow only can calculate until 32 bits"
+// number max that calculater can calculate  after that right "overflow only can calculate until 32 bits"
 
  function insert(num){
-                document.form.textview.value = document.form.textview.value+num
+      if((document.form.textview.value+num)>2147483647 || (document.form.textview.value+num)<-2147483647){
+                document.form.textview.value = "Overflow!max is 32bits"
+                
             }
+     else{
+           if((num=="+" || num=="-"|| num=="*"|| num=="."|| num=="%"|| num=="/")){
+              
+               document.form.textview.value = document.form.textview.value+num
+            }
+         else{
+         if((eval(document.form.textview.value+num))>2147483647 || (eval(document.form.textview.value+num))<-2147483647){
+                document.form.textview.value = "Overflow!max is 32bits"
+               }
+             else{
+                 document.form.textview.value = document.form.textview.value+num
+             }
+               }
+     }
+ }
             function equal(){
                 var exp = document.form.textview.value
+              
                 if(exp){
-                    document.form.textview.value = eval(exp)
-                }
+                    try {
+                        eval(exp)
+                        } catch (e) {
+                            
+                            if (e instanceof SyntaxError) {
+                                
+                                exp = "Impossible to calculate!"
+                                document.form.textview.value = exp
+                            }
+                            
+                            
+                            
+                        }
+                
+                   document.form.textview.value = eval(exp) 
+                
+                  }  
             }
             function clean(){
                 document.form.textview.value = ""
