820d659acd56f85b68b4bc3d1e9b62bf75a4ac2e
NacfSilva <nacf.silva@gmail.com>
2020-02-06 23:53:00 +0000

    Add files via upload

--- Resources/Js/control.js
+++ Resources/Js/control.js
@@ -11,13 +11,28 @@
                document.form.textview.value = document.form.textview.value+num
             }
          else{
+              try {
+                        eval(document.form.textview.value+num)
+                        } catch (e) {
+                            if (e instanceof SyntaxError) {
+                                exp = "Impossible to calculate!"
+                                document.form.textview.value = exp
+                            }else {
+        throw( e );
+    }
+                             
+                            
+                            
+                        }
+            
          if((eval(document.form.textview.value+num))>2147483647 || (eval(document.form.textview.value+num))<-2147483647){
                 document.form.textview.value = "Overflow! Max is 32bits."
                }
              else{
                  document.form.textview.value = document.form.textview.value+num
              }
-               }
+                 }
+               
      }
  }
             function equal(){
@@ -33,12 +48,13 @@
                                 exp = "Impossible to calculate!"
                                 document.form.textview.value = exp
                             }
-                             
+                             else {
+        throw( e );
+    }
                             
                             
                         }
-                
-                   document.form.textview.value = eval(exp) 
+            
                 
                   }  
             }
